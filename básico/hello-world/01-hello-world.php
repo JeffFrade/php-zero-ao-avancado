899dfbf9a659b8761493dc08604a2f28c73f4ad1
Jefferson Frade <jefferrson.frade@gmail.com>
2022-04-23 23:58:45 -0300

    Adicionado a parte de Hello World

---
+++ básico/hello-world/01-hello-world.php
@@ -0,0 +1,19 @@
+<?php
+
+// Comentário de Linha
+
+# Comentário de Linha
+
+/*
+Comentário de Bloco 
+*/
+
+// <?php = Abertura de um script PHP
+
+echo "Hello World!";
+
+// echo = Exibe um texto ou valor na tela.
+// As Strings (Textos) podem ser colocadas entre "aspas dupla" ou 'aspas simples'.
+// PHP utiliza ; ao final de cada instrução.
+// Por padrão, deixa-se uma linha em branco ao final do script PHP (Caso ele vá até o fim do arquivo).
+/* Caso o script PHP vá até o fim do arquivo, não é necessário o fechamento (?>).
---
+++ básico/hello-world/02-echo-e-print.php
@@ -0,0 +1,9 @@
+<?php
+
+// No PHP há 2 jeitos de se exibir um valor na tela, existe a instrução echo (A mais utilizada) e a instrução print (Menos utilizada).
+// Abaixo, vamos mostrar como utilizar essas 2 instruções.
+
+echo "Estou utilizando o echo";
+// PHP_EOL = PHP End Of Line => Basicamente funciona como uma quebra de linha
+echo PHP_EOL;
+print "Estou utilizando o print";
